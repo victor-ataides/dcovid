fc495bb686affa4032b477bf6ef0677e27999c64
Victor <55414928+victorleles12@users.noreply.github.com>
2020-07-15 20:41:18 -0300

    primeiro commit app dcovid, contem a tela inicial

---
+++ app/src/main/java/com/example/dcovid/telaInicial.java
@@ -0,0 +1,14 @@
+package com.example.dcovid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+}
